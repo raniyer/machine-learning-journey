432627079ecb15006d1ebc5ada970b3428aa274e
Raniyer <nidhiraniyer@gmail.com>
2020-03-09 17:06:35 +0530

    Data Preprocessing

---
+++ datapreprocessing(me).py
@@ -0,0 +1,22 @@
+#Data Preprocessing
+
+#importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('50_Startups.csv') 
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 4].values
+
+
+#splitting into test train
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+"""#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
